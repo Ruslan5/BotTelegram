5978a9b6e740cfeafe1205f7ada7b0cc5be27484
Feiron <rm2rm@mail.ru>
2019-01-11 16:24:51 +0200

    second commit

--- src/main/java/ua/com/feiron/bot/Bot.java
+++ src/main/java/ua/com/feiron/bot/Bot.java
@@ -1,5 +1,6 @@
 package ua.com.feiron.bot;
 
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
  * Created by f on 27.11.2017.
  */
 public class Bot extends TelegramLongPollingBot {
+    private static final Logger LOGGER = Logger.getLogger(Bot.class.getName());
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -17,7 +19,13 @@ public class Bot extends TelegramLongPollingBot {
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
             if (message.getText().equals("/help")) {
-                sendMsg(message, "Привет, что вас интересует?");
+                sendMsg(message, "Это тестовый режим Feiron Бота. Подписывайтесь на наш telegram - канал. Вскором времени" +
+                        " Бот заработает в функциональном режиме. У Вас появится возмоность: " +
+                        "1)Ознакомиться с ассортиментом нашей продукции; " +
+                        "2)Узнать о наличии металла; " +
+                        "3)Узнать стоимость на текущий момент времени; " +
+                        "4)Получить ответы на многие вопросы, связанные с кровлей. И еще много интересного... . " +
+                        "До скорой встречи! С Ув., FEIRON Bot");
             }
             else if (message.getText().equalsIgnoreCase("профнастил")) {
                 sendMsg(message, "Какой? У нас имеется: 'С10', 'С20', 'С35', 'С44', 'С60', 'С75' 'url'");
@@ -37,11 +45,14 @@ public class Bot extends TelegramLongPollingBot {
 
 
     private void sendMsg(Message message, String s) {
+
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(s);
+        LOGGER.info(message);
+        LOGGER.info(message.getText());
         try {
             sendMessage(sendMessage);
         } catch (TelegramApiException e) {
--- src/main/java/ua/com/feiron/bot/Main.java
+++ src/main/java/ua/com/feiron/bot/Main.java
@@ -1,18 +1,28 @@
 package ua.com.feiron.bot;
 
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 import java.net.MalformedURLException;
+import java.util.Scanner;
 
 /**
  * Created by f on 14.12.2017.
  */
 public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
     public static void main(String[] args) throws MalformedURLException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Запуск");
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+//        LOGGER.getLogger(telegramBotsApi.toString());
+        LOGGER.info(telegramBotsApi);
+
 
         try {
             telegramBotsApi.registerBot(new Bot());
